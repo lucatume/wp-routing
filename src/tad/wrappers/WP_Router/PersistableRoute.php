16d0dfcb6879baee1c58457bdd7d501d96fd7af8
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-06-07 13:45:01 +0200

    added the explicit __construct method and the Option dependency

--- src/tad/wrappers/WP_Router/PersistableRoute.php
+++ src/tad/wrappers/WP_Router/PersistableRoute.php
@@ -2,11 +2,23 @@
 namespace tad\wrappers\Wp_Router;
 
 use \tad\wrappers\WP_Router\Route;
+use \tad\wrappers\Option;
 
 class PersistableRoute extends Route
 {
+    public function __construct(FunctionsAdapter $f = null, Option $option = null){
+        if (is_null($option)) {
+            $option = Option::on('WP_Router_routes_information');
+        }
+        $this->option = $option;
+        parent::__construct($f);
+    }
     private static function actOnRoute()
     {
 
+    }
+    public function shouldBePersisted(){
+        $this->args['shouldBePersisted'] = true;
+        return $this; 
     }
 }
