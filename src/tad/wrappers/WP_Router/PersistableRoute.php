08a3e355c27450fe50360871c746d402925b97db
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-06-07 13:13:00 +0200

    added the PersistableRoute bare bones

---
+++ src/tad/wrappers/WP_Router/PersistableRoute.php
@@ -0,0 +1,12 @@
+<?php
+namespace tad\wrappers\Wp_Router;
+
+use \tad\wrappers\WP_Router\Route;
+
+class PersistableRoute extends Route
+{
+    private static function actOnRoute()
+    {
+
+    }
+}
